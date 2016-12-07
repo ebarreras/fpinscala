19c83d1c18d616c2323efc058c31e062079ae0f4
Ernesto Barreras <ernesto.barreras@capco.com>
2016-12-07 15:23:19 -0500

    Rand's map2, sequence

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -59,16 +59,28 @@ object RNG {
   }
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) =
-    (0 to count).foldLeft((Nil: List[Int], rng)) {
+    (0 until count).foldLeft((Nil: List[Int], rng)) {
       case ((l, r), _) => {
         val (i, r2) = r.nextInt
         (i :: l, r2)
       }
     }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def double_1: Rand[Double] =
+    map(nonNegativeInt)(_ /(Int.MaxValue.toDouble + 1))
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a,b), rng3)
+    }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldLeft(unit(Nil: List[A]))((listRand, aRand) => map2(aRand, listRand)(_ :: _))
+
+  def intsViaSequence(count: Int)(rng: RNG): (List[Int], RNG) =
+    sequence(List.fill(count)(int))(rng)
 
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
--- exercises/src/main/scala/fpinscala/state/worksheet.sc
+++ exercises/src/main/scala/fpinscala/state/worksheet.sc
@@ -3,3 +3,5 @@ import fpinscala.state.RNG._
 val rng = Simple(12345L)
 
 ints(5)(rng)
+intsViaSequence(5)(rng)
+
